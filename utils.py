cfa55269e164c4a1fe068fd7ba2456f1a7da0618
PabloAndresCQ <pablo.andresmartinez@quantinuum.com>
2025-06-21 05:59:04 -0700

    Added utils to get fidelities from metric files

--- utils.py
+++ utils.py
@@ -28,5 +28,21 @@ if __name__ == "__main__":
       circ_with_fidelity.sort(key=lambda p: p[1])
       for circ, fid, t in circ_with_fidelity:
         print(f"{circ}: {fid} (sim: {round(t,2)}s)")
+    case "--get-fidelity":
+      circ_list = Path(sys.argv[2])
+      metric_path = Path(sys.argv[3]) / "metrics"
+      with open(circ_list, "r") as f:
+        lines = f.readlines()
+      circs = list(map(lambda s: s[:-1], lines))
+      circ_with_fidelity = list()  # A list of tuples
+      for filepath in metric_path.iterdir():
+        circname = str(filepath).split("/")[-1].split(".txt")[0]
+        if circname in circs:
+          mirror_fidelity = get_metric("mirror_fidelity", filepath)
+          sim_time = get_metric("simulation_time", filepath)
+          circ_with_fidelity.append((circname, mirror_fidelity, sim_time))
+      circ_with_fidelity.sort(key=lambda p: p[1])
+      for circ, fid, t in circ_with_fidelity:
+        print(f"{circ}: {fid} (sim: {round(t,2)}s)")
     case _:
       print(f"Unrecognised option {option}")
